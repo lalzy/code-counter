efc52ca03148f066735c6082a1de375f79674984
skn <svenyk@gmail.com>
2025-02-18 12:59:17 +0100

    Satte ekte main funksjon
    Lar oss gi directory som parameter (b.l.a; drag-drop dir oppå executable).

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-LoadProjectFiles lpf = new LoadProjectFiles("/home/skn/coding-projects/common-lisp/yggdrasil2");
+LoadProjectFiles lpf = new LoadProjectFiles("/home/skn/coding-projects/common-lisp/yggdrasil");
 lpf.addFileType("*.lisp");
 lpf.addFileType("*.asd");
 lpf.AddFolderToIgnore(".git");
